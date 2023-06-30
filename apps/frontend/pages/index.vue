da09af3dddcf764437abd950543f6cef7d060333
maximgofman <maxim.gofman@wambo-agency.com>
2023-06-30 19:34:01 +0200

    Updated Frontend

--- apps/frontend/pages/index.vue
+++ apps/frontend/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Home page</h1>
+  </div>
 </template>
-
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+export default class Index extends Vue {
+  // ...
+}
+
 </script>
